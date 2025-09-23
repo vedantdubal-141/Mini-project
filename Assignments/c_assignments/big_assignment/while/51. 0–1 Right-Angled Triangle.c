f450b86abaec830f23cfbe5137f55aeb173e439d
vedantdubal-141 <vedant.dubal.cg@gmail.com>
2025-09-23 17:30:21 +0900

    optimizing performance in 51. 0–1 Right-Angled Triangle.c

---
+++ Assignments/c_assignments/big_assignment/while/51. 0–1 Right-Angled Triangle.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int i = 1;
+
+    while (i <= n) {
+        int j = 1;
+        while (j <= i) {
+            if ((i+j) % 2 == 0) { printf("1");}
+            else { printf("0");}
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+
+    return 0;
+}
