e632af827ca9869fd58e6232059f891a7e6b42e6
vedantdubal-141 <vedant.dubal.cg@gmail.com>
2025-10-03 09:29:27 +0900

    optimizing performance in square.c

---
+++ class/C/Class_5/square.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        int square = i * i;
+        printf("The square of %d is %d\n", i, square);
+    }
+
+    return 0;
+}
